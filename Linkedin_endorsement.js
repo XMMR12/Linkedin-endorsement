b25d0f075ec135e8d83f4574a375e8614def6336
XMMR12 <31443996+XMMR12@users.noreply.github.com>
2021-06-16 22:07:26 +0200

    Creating the script code to use as injection code
    Creating the script code to use as injection code

---
+++ Linkedin_endorsement.js
@@ -0,0 +1,3 @@
+/*Works when you open someones profile in desktop view*/
+/* can work from mobiles in desktop view*/
+endors=document.getElementsByClassName("pv-skill-category-entity__endorse-action pv-skill-entity__endorse-actions ember-view");for(x=0;x<endors.length;x++){endors[x].children[0].click();}
