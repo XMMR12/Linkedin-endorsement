f91030cf18da444cab862f7df80c534d74865523
XMMR12 <31443996+XMMR12@users.noreply.github.com>
2022-02-16 18:13:17 +0200

    Works with the latest version of linkedin updated on feb [2022]

--- Linkedin_endorsement.js
+++ Linkedin_endorsement.js
@@ -1,3 +1,6 @@
 /*Works when you open someones profile in desktop view*/
 /* can work from mobiles in desktop view*/
-endors=document.getElementsByClassName("pv-skill-category-entity__endorse-action pv-skill-entity__endorse-actions ember-view");for(x=0;x<endors.length;x++){endors[x].children[0].click();}
+/* //Old version:
+endors=document.getElementsByClassName("pv-skill-category-entity__endorse-action pv-skill-entity__endorse-actions ember-view");for(x=0;x<endors.length;x++){endors[x].children[0].click();}*/
+/*Updated version:*/
+endors=document.getElementsByClassName("ember-view");for(x=0;x<endors.length;x++){if (endors[x].innerText=="Endorse"){endors[x].click();}}
